3f7b9adc2d5f1376319761b9a1bc48d4fd20194a
Veronica <veronicasmigielski@gmail.com>
2021-09-08 19:57:08 +0200

    added new files

---
+++ challenge.js
@@ -0,0 +1,8 @@
+// Start a new file
+// Congratulations John Smith. You have solved your first JavaScript Challenge
+let firstName = "John"
+let lastName = "Smith"
+let fullName = firstName + " " + lastName
+let secondSentence = "You have solved your first JavaScript Challenge"
+
+console.log('Congratulations ' + fullName + '. ' + secondSentence)
---
+++ variablestrings.js
@@ -0,0 +1,19 @@
+// This is a single line comment
+/* Multi-line 
+    comment */
+
+// First greeting variable
+let myGreeting = "Hello from JavaScript!" // This is my first variable
+
+// You can declare a variable & then define it later in the program
+let my_greeting
+let greeting
+
+// Variable to hold first name
+let firstName
+firstName = "Veronica"
+
+// Create the output
+console.log(myGreeting)
+console.log(firstName)
+console.log("Hello " + firstName + "! " + myGreeting)
