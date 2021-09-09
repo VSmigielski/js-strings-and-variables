a0d67246b93fa494372587172aa87926a2c9dc2d
Veronica <veronicasmigielski@gmail.com>
2021-09-09 10:52:14 +0200

    added new file for video

---
+++ variablenumbers.js
@@ -0,0 +1,52 @@
+// Create numeric variable
+// Integer
+let num1 = 8
+// Float
+let num2 = 8.5
+
+// Output
+console.log('The number in the num1 variable is ' + num1)
+
+/* + is addition operator 
+- is the subtraction operator
+* is the multiplication operator
+/ is the division operator */
+
+// Create a variable to hold first number
+let number1 = 8
+
+// Create a variable to hold second number
+let number2 = 2
+
+// Create a variable to add the two numbers
+let add = number1 + number2
+
+// Create a variable to subtract the two numbers
+let subtract = number1 - number2
+
+// Create a variable to multiply the two numbers
+let multiply = number1 * number2
+
+// Create a variable to divide the two numbers
+let divide = number1 / number2
+
+// Create a variable to calculate the result
+let calculate = number1 + number2 / number2
+
+// override natural order of ops
+let calculate2 = (number1 + number2) / number2
+
+// Declare a const
+const number3 = 8
+number3 = 2
+
+// Output
+console.log('Adding number1 and number 2 results in ' + add)
+console.log('Subtracting number1 and number 2 results in ' + subtract)
+console.log('Multiplying number1 and number 2 results in ' + multiply)
+console.log('Dividing number1 and number 2 results in ' + divide)
+
+console.log('Adding number1 and number 2 results in ' + number1 + number2)
+console.log('Adding number1 and number 2 results in ' + (number1 + number2))
+
+console.log('The result is ' + calculate)
